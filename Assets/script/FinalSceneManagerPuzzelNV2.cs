987ebbc81a3a4b16769ffcdb50c07a1a45e14f77
danielaSilverdeusquechato <danielalgsilva@aecastrodaire.com>
2025-05-28 19:54:47 +0100

    nivel2 de puzzel

---
+++ Assets/script/FinalSceneManagerPuzzelNV2.cs
@@ -0,0 +1,26 @@
+using System.Collections; 
+using System.Collections.Generic; 
+using TMPro; 
+using UnityEngine; 
+using UnityEngine.SceneManagement; 
+public class FinalSceneManagerPuzzelNV2 : MonoBehaviour 
+{ 
+    [SerializeField] 
+    private TMP_Text rightAnswersText; 
+ 
+    [SerializeField] 
+    private TMP_Text wrongAnswersText; 
+ 
+    public void Start() 
+    { 
+        rightAnswersText.text = GameManagerPuzzelNV2.GetRightAnswer().ToString(); 
+        wrongAnswersText.text = GameManagerPuzzelNV2.GetWrongAnswer().ToString(); 
+    } 
+ 
+    public void TestAgain() 
+    { 
+        GameManagerPuzzelNV2.Reset(); 
+        SceneManager.LoadScene("MainScene"); 
+    } 
+}
+
